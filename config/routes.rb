814a794acf9c58a27638ba102c944d01d688443b
maaoo <mauro001989@gmail.com>
2020-05-26 11:34:36 -0500

    hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   api_version(module: 'api/v1', path: { value: 'api/v1' }, defaults: { format: :json }) do
     resources :users
 
-    resources :projects
-    resources :tasks do
+    resources :projects do
+      resources :tasks
+    end
+    resources :tasks, only: [] do
       resources :comments
     end
 
